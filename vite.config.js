0687f05d7551f7375efec5b45d3ab1abd7ba6245
AlexZuried <techr887@gmail.com>
2026-04-13 20:38:52 +0530

    Add build options to vite.config.js
    Added build configuration to optimize performance and manage chunk sizes.

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,20 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
+    build: {
+        // 1. Disable sourcemaps to save memory and speed up 'transforming'
+        sourcemap: false,
+        // 2. Reduce the chunk size warning limit
+        chunkSizeWarningLimit: 1600,
+        rollupOptions: {
+            output: {
+                // 3. Manual chunking to prevent one giant file from hanging the build
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                },
+            },
+        },
+    },
 });
